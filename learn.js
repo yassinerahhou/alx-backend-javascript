d6b35eafc01190d472338ef8caa64a37ff10be44
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-01-26 19:49:07 +0100

    added packages

--- learn.js
+++ learn.js
@@ -1,3 +1,24 @@
+// var
+//         - Function Scope
+//         - Can be redeclared
+//         - Can assign different value
+//         - When you call variable before declare it var will return undefined
+//         - It generates property with the name of the variable inside the window object but var shouldn't be inside any scope, so it's not organized and accessable from everywhere
+
+//     let
+//         - Block Scope
+//         - Cannot be redeclared
+//         - Can assign different value
+//         - When you call variable before declare it let will return error which is more logical
+//         - It doesn't generates property with the name of the variable inside the window object and it will return undefined
+
+//     const
+//         - Block Scope
+//         - Cannot be redeclared
+//         - Cannot assign new value
+//         // - When you call variable before declare it let will return error which is more logical
+// - It doesn't generates property with the name of the variable inside the window object and it will return undefined
+
 function verTest() {
   var x = 1;
   if (true) {
